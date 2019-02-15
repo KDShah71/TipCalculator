608403b5655edbeb79b943d301df5c498977e2f9
Kundan Shah <kundanshah71@gmail.com>
2019-02-15 16:11:50 +0545

    TipsCalculator task done

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-function calculate() {
+function calculateTip() {
   let billAmount = document.getElementById("billAmount").value;
+  if (billAmount == "") {
+    document.getElementById("billAmount").focus();
+    return false;
+  }
+
   let tipPercent = document.getElementById("tipPercent").value;
+  if (tipPercent == "") {
+    document.getElementById("tipPercent").focus();
+    return false;
+  }
   let numOfPeople = document.getElementById("numOfPeople").value;
+  if (numOfPeople == "") {
+    document.getElementById("numOfPeople").focus();
+    return false;
+  }
 
   let splitAmount = (billAmount * (tipPercent / 100)) / numOfPeople;
 
   document.getElementById(
     "result"
-  ).innerHTML = `<h3>Tip Amount $ ${splitAmount} per person</h3>`;
+  ).innerHTML = `<h3>Tip Amount $ ${splitAmount} / person</h3>`;
 }
